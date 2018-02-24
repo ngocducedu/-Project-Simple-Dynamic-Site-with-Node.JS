e315fae00185b20d5d86dcd5aef427939879961e
Bui Ngoc Duc <superufom@gmail.com>
2018-02-24 11:03:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
   router.home(request, response);
   router.user(request, response);
 }).listen(3000);
-console.log('Server running at http://<workspace-url>/');
+console.log('Server running at localhost:3000/');
 
 
 
